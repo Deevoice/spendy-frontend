68f74342922f37a4b1c47bf6edd9150d18d9738a
D0Nater <denaddressforwork@gmail.com>
2025-06-18 05:47:57 +0300

    refactor: simplify Dockerfile and entrypoint script, remove nginx configuration

--- deployment/entrypoint.sh
+++ deployment/entrypoint.sh
@@ -1,15 +1,3 @@
 #!/bin/sh
 
-for i in $(env | grep NEXT_)
-do
-  key=$(echo $i | cut -d '=' -f 1)
-  value=$(echo $i | cut -d '=' -f 2-)
-  echo $key=$value
-
-  # sed JS and CSS only
-  find /usr/share/nginx/html -type f -name '*.js' -exec sed -i "s;%${key}%;${value};g" '{}' +
-done
-
-echo 'done'
-
-nginx -g 'daemon off;'
+npm run dev
