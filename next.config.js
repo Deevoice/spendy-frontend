a80ec7bed1a96fe4f92554731c507a4ba1c30339
D0Nater <denaddressforwork@gmail.com>
2025-06-18 06:43:07 +0300

    fix: add image remote patterns to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    /* config options here */
+    images: {
+        remotePatterns: [new URL('https://api.spendy.deevoice.ru/**')],
+    },
 };
 
 module.exports = nextConfig; 
